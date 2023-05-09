eeefd723f6b7e44da9b0e2fc33963e06af3e1564
partisan777 <103387991+partisan777@users.noreply.github.com>
2023-05-09 12:35:37 +0300

    feat(TGG-19): страница лидерборда

--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,3 +1,88 @@
+import {  Table  } from "antd";
+
+
+const data = [
+  {
+    position: "1",
+    display_name: "Игрок1",
+    totalScore: "100",
+    countGame: "10",
+    winnerCount: "10",
+    defeatCount: "10",
+    winRate: "100"
+  },
+  {
+    position: "2",
+    display_name: "Игрок2",
+    totalScore: "90",
+    countGame: "10",
+    winnerCount: "9",
+    defeatCount: "1",
+    winRate: "90"
+  },
+  {
+    position: "3",
+    display_name: "Игрок3",
+    totalScore: "80",
+    countGame: "10",
+    winnerCount: "8",
+    defeatCount: "2",
+    winRate: "80"
+  },
+  {
+    position: "4",
+    display_name: "Игрок4",
+    totalScore: "70",
+    countGame: "10",
+    winnerCount: "7",
+    defeatCount: "3",
+    winRate: "70"
+  },
+];
+
 export const Leaderboard = () => {
-  return <div>Leaderboard</div>
+  const columns = [
+    {
+      title: 'Позиция',
+      dataIndex: 'position',
+      key: 'position',      
+    },
+    {
+      title: 'Никнейм',
+      dataIndex: 'display_name',
+      key: 'display_name',
+    },
+    {
+      title: 'Всего очков',
+      dataIndex: 'totalScore',
+      key: 'totalScore',
+    },
+    {
+      title: 'Всего матчей',
+      key: 'countGame',
+      dataIndex: 'countGame',
+    },
+    {
+      title: 'Побед',
+      key: 'winnerCount',
+      dataIndex: 'winnerCount',
+    },
+    {
+      title: 'Поражений',
+      key: 'defeatCount',
+      dataIndex: 'defeatCount',
+    },
+    {
+      title: 'Винрейт',
+      key: 'winRate',
+      dataIndex: 'winRate',
+    },
+  ];
+  return <Table columns={columns} dataSource={data} />; 
 }
+
+
+
+
+
+
