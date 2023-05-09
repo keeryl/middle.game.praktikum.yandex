fe3c4448b75ea95ba00b9608b8aeed89b926cc6a
Igor Pronin <112540409+go-pro0000@users.noreply.github.com>
2023-05-09 13:01:50 +0300

    feat(TGG-13): страницы ошибок

--- packages/client/src/pages/NotFound/NotFound.tsx
+++ packages/client/src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom'
+import styles from "../ServerError/styles.module.scss"
+
 export const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <img src="/public/notFoundPage/notFound.svg" alt="" className={styles.container__image} />
+
+        <h1 className={styles.container__title}>Не туда попали</h1>
+
+        <Link className={styles.container__link} to="/">
+          На главную
+        </Link>
+      </div>
+    </div>
+  )
 }
--- packages/client/src/pages/ServerError/ServerError.tsx
+++ packages/client/src/pages/ServerError/ServerError.tsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom'
+import styles from "./styles.module.scss"
+
 export const ServerError = () => {
-  return <div>ServerError</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <img src="/public/serverErrorPage/serverError.svg" alt="" className={styles.container__image} />
+
+        <h1 className={styles.container__title}>Что-то пошло не так :(</h1>
+
+        <Link className={styles.container__link} to="/">
+          На главную
+        </Link>
+      </div>
+    </div>
+  )
 }
