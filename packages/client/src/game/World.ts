6b00f9f444bf85493de282c60e7f902c666b33c9
go-pro0000 <igor.pronin.2000@acgnn.ru>
2023-05-19 11:56:40 +0300

    движение танчика с учетом других танок, анимация полета пули

--- packages/client/src/game/World.ts
+++ packages/client/src/game/World.ts
@@ -1,200 +1,199 @@
-// import { View } from './View'
-// import { Enemy } from './Enemy'
-// import { Player } from './Player'
-// import { Bullet } from './Bullet'
-// import { getInitialPositions } from './config'
-// import { Tank } from './Tank'
+import { View } from './View'
+import { Enemy } from './Enemy'
+import { Player } from './Player'
+import { Bullet } from './Bullet'
+import { getInitialPositions } from './config'
+import { Tank } from './Tank'
 
-// export class World {
-//   canvas: HTMLCanvasElement
-//   context: CanvasRenderingContext2D
-//   view: View
-//   player: Player
-//   enemies: Enemy[]
-//   bullets: Bullet[]
+export class World {
+  canvas: HTMLCanvasElement
+  context: CanvasRenderingContext2D
+  view: View
+  player: Player
+  enemies: Enemy[]
+  bullets: Bullet[]
 
-//   constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
-//     this.canvas = canvas
-//     this.context = context
-//     this.loop = this.loop.bind(this)
-//     this.view = new View(canvas, context)
-//     this.view.init()
-//     this.player = {} as Player
-//     this.enemies = []
-//     this.bullets = []
-//     this.spawnPlayer(canvas, context)
-//     this.spawnEnemies(canvas, context)
-//     this.setEventListeners()
-//   }
+  constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+    this.canvas = canvas
+    this.context = context
+    this.loop = this.loop.bind(this)
+    this.view = new View(canvas, context)
+    this.view.init()
+    this.player = {} as Player
+    this.enemies = []
+    this.bullets = []
+    this.spawnPlayer(canvas, context)
+    this.spawnEnemies(canvas, context)
+    this.setEventListeners()
+  }
 
-//   public setEventListeners() {
-//     document.addEventListener('keydown', e => {
-//       if (e.code === 'ArrowUp') {
-//         this.player.moveUp()
-//       }
-//       if (e.code === 'ArrowDown') {
-//         this.player.moveDown()
-//       }
-//       if (e.code === 'ArrowLeft') {
-//         this.player.moveLeft()
-//       }
-//       if (e.code === 'ArrowRight') {
-//         this.player.moveRight()
-//       }
+  public setEventListeners() {
+    document.addEventListener('keydown', e => {
+      if (e.code === 'ArrowUp') {
+        this.player.moveUp()
+      }
+      if (e.code === 'ArrowDown') {
+        this.player.moveDown()
+      }
+      if (e.code === 'ArrowLeft') {
+        this.player.moveLeft()
+      }
+      if (e.code === 'ArrowRight') {
+        this.player.moveRight()
+      }
 
-//       if (e.code === 'ArrowUp' || e.code === 'ArrowDown' || e.code === 'ArrowLeft' || e.code === 'ArrowRight') {
-//         for (let i = 0; i < this.enemies.length; i++) {
-//           if (this.identifyHits(this.enemies[i], this.player, this.player.direction)) {
-//             console.log("врезались");
-//             const savedPlayerDirection = this.player.direction
-//             this.tankGoBack();
-//             this.player.direction = savedPlayerDirection;
-//             break;
-//           }
-//         }
-//         this.rerender();
-//       }
+      if (e.code === 'ArrowUp' || e.code === 'ArrowDown' || e.code === 'ArrowLeft' || e.code === 'ArrowRight') {
+        for (let i = 0; i < this.enemies.length; i++) {
+          if (this.identifyHits(this.enemies[i], this.player, this.player.direction)) {
+            const savedPlayerDirection = this.player.direction
+            this.tankGoBack();
+            this.player.direction = savedPlayerDirection;
+            break;
+          }
+        }
+        this.rerender();
+      }
 
-//       if (e.code === 'Space') {
-//         this.spanBullet(this.canvas, this.context)
-//       }
-//     })
-//   }
+      if (e.code === 'Space') {
+        this.spanBullet(this.canvas, this.context)
+      }
+    })
+  }
 
-//   private spawnPlayer(
-//     canvas: HTMLCanvasElement,
-//     context: CanvasRenderingContext2D
-//   ) {
-//     this.player = new Player(
-//       canvas,
-//       context,
-//       getInitialPositions().PLAYER
-//     )
-//     this.player.render()
-//   }
+  private spawnPlayer(
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D
+  ) {
+    this.player = new Player(
+      canvas,
+      context,
+      getInitialPositions().PLAYER
+    )
+    this.player.render()
+  }
 
-//   private spawnEnemies(
-//     canvas: HTMLCanvasElement,
-//     context: CanvasRenderingContext2D
-//   ) {
-//     getInitialPositions().ENEMIES.forEach(position => {
-//       this.enemies.push(new Enemy(canvas, context, position))
-//     })
+  private spawnEnemies(
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D
+  ) {
+    getInitialPositions().ENEMIES.forEach(position => {
+      this.enemies.push(new Enemy(canvas, context, position))
+    })
 
-//     this.enemies.forEach(enemy => enemy.render())
-//   }
+    this.enemies.forEach(enemy => enemy.render())
+  }
 
-//   private spanBullet(
-//     canvas: HTMLCanvasElement,
-//     context: CanvasRenderingContext2D
-//   ) {
-//     const bulletStartPosition = this.createBulletStartPosition();
-//     const bullet = new Bullet(canvas, context, bulletStartPosition);
-//     this.bullets.push(bullet)
-//     this.animateBullet(bullet, this.player.direction);
-//   }
+  private spanBullet(
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D
+  ) {
+    const bulletStartPosition = this.createBulletStartPosition();
+    const bullet = new Bullet(canvas, context, bulletStartPosition);
+    this.bullets.push(bullet)
+    this.animateBullet(bullet, this.player.direction);
+  }
 
-//   public animateBullet(bullet: Bullet, bulletDirection: string) {
-//     bullet[bulletDirection]();
+  public animateBullet(bullet: Bullet, bulletDirection: string) {
+    // bullet[bulletDirection]();
+    switch (bulletDirection) {
+      case ('moveUp'):
+        bullet.moveUp();
+        break;
 
-//     for (let i = 0; i < this.enemies.length; i++) {
-//       if (this.identifyHits(this.enemies[i], bullet, bulletDirection)) {
-//         bullet.flying = false;
-//         this.enemies.splice(i, 1);
-//         break;
-//       }
-//     }
+      case ('moveDown'):
+        bullet.moveDown();
+        break;
 
-//     this.rerender()
+      case ('moveLeft'):
+        bullet.moveLeft();
+        break;
 
-//     if (bullet.flying) {
-//       requestAnimationFrame(this.animateBullet.bind(this, bullet, bulletDirection));
-//     }
-//   };
+      case ('moveRight'):
+        bullet.moveRight();
+        break;
+    }
 
-//   private identifyHits(enemy: Enemy, object: Bullet | Tank, direction: string) {
-//     const objectClass = object.constructor.name === 'Bullet' ? Bullet : Tank;
-//     if (object.position.y < enemy.position.y + Tank.size && object.position.y + objectClass.size > enemy.position.y && object.position.x + objectClass.size > enemy.position.x && enemy.position.x + Tank.size > object.position.x) {
-//       return true
-//     }
-//     if (direction === 'moveUp') {
-//       if (object.position.y < enemy.position.y + Tank.size && object.position.y + objectClass.size > enemy.position.y && object.position.x + objectClass.size > enemy.position.x && object.position.x > enemy.position.x + Tank.size) {
-//         return true;
-//       }
-//     } else if (direction === 'moveDown') {
-//       if (object.position.y + objectClass.size > enemy.position.y && object.position.y < enemy.position.y + Tank.size && object.position.x + objectClass.size > enemy.position.x && object.position.x > enemy.position.x + Tank.size) {
-//         return true;
-//       }
-//     } else if (direction === 'moveLeft') {
-//       if (object.position.x < enemy.position.x + Tank.size && object.position.y + objectClass.size > enemy.position.y && object.position.y + objectClass.size < enemy.position.y && object.position.y > enemy.position.y + Tank.size) {
-//         return true;
-//       }
-//     } else if (direction === 'moveRight') {
-//       if (object.position.x + objectClass.size > enemy.position.x && object.position.x > enemy.position.x + objectClass.size && object.position.y + objectClass.size < enemy.position.y && object.position.y > enemy.position.y + Tank.size) {
-//         return true;
-//       }
-//     }
-//   }
+    for (let i = 0; i < this.enemies.length; i++) {
+      if (this.identifyHits(this.enemies[i], bullet, bulletDirection)) {
+        bullet.flying = false;
+        this.enemies.splice(i, 1);
+        break;
+      }
+    }
 
-//   private createBulletStartPosition() {
-//     if (this.player.direction === 'moveUp') {
-//       return {
-//         x: this.player.position.x + (Tank.size - Bullet.size) / 2,
-//         y: this.player.position.y,
-//       }
-//     } else
-//       if (this.player.direction === 'moveDown') {
-//         return {
-//           x: this.player.position.x + (Tank.size - Bullet.size) / 2,
-//           y: this.player.position.y + Tank.size,
-//         }
-//       } else
-//         if (this.player.direction === 'moveLeft') {
-//           return {
-//             x: this.player.position.x,
-//             y: this.player.position.y + (Tank.size - Bullet.size) / 2,
-//           }
-//         } else {
-//           return {
-//             x: this.player.position.x + Tank.size,
-//             y: this.player.position.y + (Tank.size - Bullet.size) / 2,
-//           }
-//         }
-//   }
+    this.rerender()
 
-//   private tankGoBack() {
-//     switch (this.player.direction) {
-//       case ('moveUp'):
-//         this.player.moveDown();
-//         break;
+    if (bullet.flying) {
+      requestAnimationFrame(this.animateBullet.bind(this, bullet, bulletDirection));
+    }
+  };
 
-//       case ('moveDown'):
-//         this.player.moveUp();
-//         break;
+  private identifyHits(enemy: Enemy, object: Bullet | Tank, direction: string) {
+    const objectClass = object.constructor.name === 'Bullet' ? Bullet : Tank;
+    if (object.position.y < enemy.position.y + Tank.size && object.position.y + objectClass.size > enemy.position.y && object.position.x + objectClass.size > enemy.position.x && enemy.position.x + Tank.size > object.position.x) {
+      return true
+    }
+  }
 
-//       case ('moveLeft'):
-//         this.player.moveRight();
-//         break;
+  private createBulletStartPosition() {
+    if (this.player.direction === 'moveUp') {
+      return {
+        x: this.player.position.x + (Tank.size - Bullet.size) / 2,
+        y: this.player.position.y,
+      }
+    } else
+      if (this.player.direction === 'moveDown') {
+        return {
+          x: this.player.position.x + (Tank.size - Bullet.size) / 2,
+          y: this.player.position.y + Tank.size,
+        }
+      } else
+        if (this.player.direction === 'moveLeft') {
+          return {
+            x: this.player.position.x,
+            y: this.player.position.y + (Tank.size - Bullet.size) / 2,
+          }
+        } else {
+          return {
+            x: this.player.position.x + Tank.size,
+            y: this.player.position.y + (Tank.size - Bullet.size) / 2,
+          }
+        }
+  }
 
-//       case ('moveRight'):
-//         this.player.moveLeft();
-//         break;
-//     }
-//   }
+  private tankGoBack() {
+    switch (this.player.direction) {
+      case ('moveUp'):
+        this.player.moveDown();
+        break;
 
-//   private rerender() {
-//     this.view.update()
-//     this.player.render()
-//     this.enemies.forEach(enemy => enemy.render())
-//     this.bullets = this.bullets.filter((item) => item.flying);
-//     this.bullets.forEach(bullet => bullet.render())
-//   }
+      case ('moveDown'):
+        this.player.moveUp();
+        break;
 
-//   public init() {
-//     requestAnimationFrame(this.loop)
-//   }
+      case ('moveLeft'):
+        this.player.moveRight();
+        break;
 
-//   private loop() {
-//     requestAnimationFrame(this.loop)
-//   }
-// }
+      case ('moveRight'):
+        this.player.moveLeft();
+        break;
+    }
+  }
+
+  private rerender() {
+    this.view.update()
+    this.player.render()
+    this.enemies.forEach(enemy => enemy.render())
+    this.bullets = this.bullets.filter((item) => item.flying);
+    this.bullets.forEach(bullet => bullet.render())
+  }
+
+  public init() {
+    requestAnimationFrame(this.loop)
+  }
+
+  private loop() {
+    requestAnimationFrame(this.loop)
+  }
+}
