49acdd48ff0aead29e542cc46f970e083fb6399e
Nikita <n173@bk.ru>
2023-05-23 17:30:06 +0300

    исправлен нейминг

--- packages/client/src/components/ServiceWorker/RegisterResponseSW.ts
+++ packages/client/src/components/ServiceWorker/RegisterResponseSW.ts
@@ -1,7 +1,7 @@
 export const registerResponseSW = (): void => {
   if (location.hostname === "localhost") return;
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw-prod.ts").then(registration => {
+    navigator.serviceWorker.register("./RegisterResponseSW.ts").then(registration => {
       const a = registration;
     }).catch(error => {
       const b = error;
