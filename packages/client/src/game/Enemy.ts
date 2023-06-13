4ff4f5dd07bc1148a47536eb88d2be0743589589
partisan777 <103387991+partisan777@users.noreply.github.com>
2023-06-13 14:54:01 -0700

    пофикшен бвг с мерцанием танков при выстрелах (#5)

--- packages/client/src/game/Enemy.ts
+++ packages/client/src/game/Enemy.ts
@@ -10,15 +10,19 @@ export class Enemy extends Tank {
     position: { x: number; y: number }
   ) {
     super(canvas, context, position)
+    this.canvas = canvas
+    this.context = context
     this.position = position
     this.direction = 'moveDown'
     this.img = new Image()
   }
 
   public render() {
+    
     this.img.src = src
     this.img.width = Tank.size
     this.img.height = Tank.size
+    this.context.drawImage(this.img, this.position.x, this.position.y)
     this.img.onload = () => {
       this.context.drawImage(this.img, this.position.x, this.position.y)
     }
