69c22b162222edfdfc63ca12a0670bd736b52812
Alena-0046 <alena-0046@yandex.ru>
2023-06-25 06:34:25 +0300

    TGG-57 - Sprint 8 - Reactions

--- packages/server/controllers/reaction.ts
+++ packages/server/controllers/reaction.ts
@@ -13,14 +13,14 @@ export function getAllReactionsForComment (req: Request, res: Response) {
 }
 
 export function createReactionForComment (req: Request, res: Response) {
-  Reaction.create(req.body)
+  const comment_id = Number(req.params.id);
+  Reaction.create({ comment_id: comment_id, ...req.body} )
     .then(reaction => res.send(reaction))
     .catch(err => console.error(err))
 }
 
 export function deleteReactionForComment (req: Request, res: Response) {
   const comment_id = Number(req.params.id);
-
   Reaction.destroy({
     where: { comment_id: comment_id, ...(req.body) }
   })
