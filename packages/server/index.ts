0978b7be72e8f81e234e101d2bbbb4d3b470b411
Nikita <n173@bk.ru>
2023-06-25 23:23:06 +0300

    Добавлена сборка сервера с ssr в Docker-контейнер

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { createServer as createViteServer } from 'vite'
 import type { ViteDevServer } from 'vite'
 
 dotenv.config()
@@ -13,73 +12,90 @@ const isDev = () => process.env.NODE_ENV === 'development'
 
 async function startServer() {
   const app = express()
-  app.use(cors())
   const port = Number(process.env.SERVER_PORT) || 3001
 
-  let vite: ViteDevServer | undefined
-  const distPath = path.dirname(require.resolve('client/dist/index.html'))
-  const srcPath = path.dirname(require.resolve('client'))
-  const ssrClientPath = require.resolve('client/ssr-dist/client.cjs')
+  try {
+    app.use(cors())
 
-  if (isDev()) {
-    vite = await createViteServer({
-      server: { middlewareMode: true },
-      root: srcPath,
-      appType: 'custom',
+    let vite: ViteDevServer | undefined
+    let distPath = ''
+    let srcPath = ''
+    let ssrClientPath = ''
+
+    if (isDev()) {
+      srcPath = path.dirname(require.resolve('client'))
+    } else {
+      distPath = path.dirname(require.resolve('./client/dist/index.html'))
+      ssrClientPath = require.resolve('./client/ssr-dist/client.cjs')
+    }
+
+    if (isDev()) {
+      const { createServer: createViteServer } = await import('vite')
+
+      vite = await createViteServer({
+        server: { middlewareMode: true },
+        root: srcPath,
+        appType: 'custom',
+      })
+
+      app.use(vite.middlewares)
+    }
+
+    app.get('/api', (_, res) => {
+      res.json('👋 Howdy from the server :)')
     })
 
-    app.use(vite.middlewares)
-  }
-
-  app.get('/api', (_, res) => {
-    res.json('👋 Howdy from the server :)')
-  })
-
-  if (!isDev()) {
-    app.use('/assets', express.static(path.resolve(distPath, 'assets')))
-  }
-
-  app.use('*', async (req, res, next) => {
-    const url = req.originalUrl
-
-    try {
-      let template: string
-
-      if (!isDev()) {
-        template = fs.readFileSync(
-          path.resolve(distPath, 'index.html'),
-          'utf-8'
-        )
-      } else {
-        template = fs.readFileSync(path.resolve(srcPath, 'index.html'), 'utf-8')
-
-        template = await vite!.transformIndexHtml(url, template)
-      }
-      let render: (url: string) => Promise<string>
-
-      if (!isDev()) {
-        render = (await import(ssrClientPath)).render
-      } else {
-        render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx')))
-          .render
-      }
-
-      const appHtml = await render(url)
-
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml)
-
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-    } catch (e) {
-      if (isDev()) {
-        vite!.ssrFixStacktrace(e as Error)
-      }
-      next(e)
+    if (!isDev()) {
+      app.use('/assets', express.static(path.resolve(distPath, 'assets')))
     }
-  })
+
+    app.use('*', async (req, res, next) => {
+      const url = req.originalUrl
+
+      try {
+        let template: string
+
+        if (!isDev()) {
+          template = fs.readFileSync(
+            path.resolve(distPath, 'index.html'),
+            'utf-8'
+          )
+        } else {
+          template = fs.readFileSync(
+            path.resolve(srcPath, 'index.html'),
+            'utf-8'
+          )
+
+          template = await vite!.transformIndexHtml(url, template)
+        }
+        let render: (url: string) => Promise<string>
+
+        if (!isDev()) {
+          render = (await import(ssrClientPath)).render
+        } else {
+          render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx')))
+            .render
+        }
+
+        const appHtml = await render(url)
+
+        const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+
+        res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      } catch (e) {
+        if (isDev()) {
+          vite!.ssrFixStacktrace(e as Error)
+        }
+        next(e)
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
   app.listen(port, () => {
     console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
   })
 }
 
-startServer()
+startServer();
