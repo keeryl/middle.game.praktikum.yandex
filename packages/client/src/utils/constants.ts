c56536125dc47e7ab3b7adfafe242967ab8380e6
Alena-0046 <121607974+Alena-0046@users.noreply.github.com>
2023-07-17 22:26:32 +0300

    Update constants.ts

--- packages/client/src/utils/constants.ts
+++ packages/client/src/utils/constants.ts
@@ -1,9 +1,9 @@
 // export const BASE_URL = 'https://ya-praktikum.tech/api/v2' // ORIGINAL BASE_URL
 
 
-export const BASE_URL = 'http://localhost:3001/api/v2'
-export const AXIOS_BASE_URL = 'http://localhost:3001/api'
-export const NODE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'http://51.250.51.19:3001/api/v2'
+export const AXIOS_BASE_URL = 'http://51.250.51.19:3001/api'
+export const NODE_URL = 'http://51.250.51.19:3001/api'
 
 
 export const BASE_URL_IMG = 'https://ya-praktikum.tech/api/v2/resources'
