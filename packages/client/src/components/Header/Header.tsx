098ed701ea73f459e18c77cd712b3ba936ed7ad6
go-pro0000 <igor.pronin@acgnn.ru>
2023-07-18 23:41:52 +0300

    add helmet

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -10,8 +10,8 @@ import {
 } from '../../store/userSelectors'
 import { logOut } from '../../store/userSlice'
 import { useCallback, useEffect, useState } from 'react'
-import light from "../../../public/img/light.svg"
-import dark from "../../../public/img/dark.svg"
+import light from "../../../public/img/light.png"
+import dark from "../../../public/img/dark.png"
 import { themeController } from '../../controllers/ThemeController'
 
 export const Header = () => {
