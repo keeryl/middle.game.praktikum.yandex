78e71b98c3a34e31783cdbdb547c499dfb7caf10
Alena-0046 <121607974+Alena-0046@users.noreply.github.com>
2023-07-18 21:24:08 +0300

    Update db.ts

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -12,9 +12,9 @@ const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } = process
 const sequelizeOptions: SequelizeOptions = {
   host: 'good-game.ya-praktikum.tech',
   port: Number(POSTGRES_PORT) || 5432, // переменная окружения недоступна, исправил позже
-  username: POSTGRES_USER || 'admin', // переменная окружения недоступна, исправил позже
-  password: POSTGRES_PASSWORD || 'root', // переменная окружения недоступна, исправил позже
-  database: POSTGRES_DB || 'good-game', // переменная окружения недоступна, исправил позже
+  username: POSTGRES_USER || 'postgres', // переменная окружения недоступна, исправил позже
+  password: POSTGRES_PASSWORD || 'postgres', // переменная окружения недоступна, исправил позже
+  database: POSTGRES_DB || 'postgres', // переменная окружения недоступна, исправил позже
   dialect: 'postgres',
 }
 
